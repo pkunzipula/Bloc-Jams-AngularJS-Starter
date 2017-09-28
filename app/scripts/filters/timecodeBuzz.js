11e3889f1c3685e292a4dd49cd7c428bbaea6b2b
James Capra <jcapra76@gmail.com>
2017-09-28 15:17:00 -0700

    Part 11 Filters complete

---
+++ app/scripts/filters/timecodeBuzz.js
@@ -0,0 +1,19 @@
+(function() {
+    function timecodeBuzz() {
+        return function(seconds) {
+            var seconds = Number.parseFloat(seconds);
+            
+            if (Number.isNaN(seconds)) {
+                return '-:--';
+            }
+            
+            var output = buzz.toTimer(seconds);
+            
+            return output;
+        };
+    }
+    
+    angular
+        .module('blocJams')
+        .filter('timecodeBuzz', timecodeBuzz]);
+})();
